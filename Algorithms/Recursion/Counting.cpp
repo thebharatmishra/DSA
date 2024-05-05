5017caf6c26ab67d7104b8c321f7c1598972a810
TheBharatMishra <mishrabm2004@gmail.com>
2024-05-05 23:19:47 +0000

    May 6, 2024, 4:49 AM

---
+++ Algorithms/Recursion/Counting.cpp
@@ -0,0 +1,14 @@
+// Necessary Imports
+#include <iostream>
+
+using namespace std;
+// Define a Class or Method
+
+int main()
+{
+    // Code here.
+    int n;
+    cin >> n;
+    print(n);
+    return 0;
+}
