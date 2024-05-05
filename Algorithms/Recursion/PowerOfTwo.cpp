f0f58340419bc60dc10306ce75314d724bbdd849
TheBharatMishra <mishrabm2004@gmail.com>
2024-05-05 23:14:33 +0000

    May 6, 2024, 4:44 AM

--- Algorithms/Recursion/PowerOfTwo.cpp
+++ Algorithms/Recursion/PowerOfTwo.cpp
@@ -7,6 +7,10 @@ int power(int n)
 {
     if (n == 0)
         return 1;
+
+    int smallerProblem = power(n - 1);
+    int biggerProblem = 2 * smallerProblem;
+    return biggerProblem;
 }
 int main()
 {
