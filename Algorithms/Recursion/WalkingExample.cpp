65bd20951af13146acf75e8705f911288b40af23
TheBharatMishra <mishrabm2004@gmail.com>
2024-05-07 14:49:17 +0000

    May 7, 2024, 8:19 PM

--- Algorithms/Recursion/WalkingExample.cpp
+++ Algorithms/Recursion/WalkingExample.cpp
@@ -5,6 +5,7 @@ using namespace std;
 // Define a Class or Method
 void reachHome(int src, int dest)
 {
+    cout << "Source: " << src << "Destination: " << dest << endl;
     if (src == dest)
         cout << "Reached the destination";
     return;
