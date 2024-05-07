2a0a03b19de647d8c66ad920af756276a1173aa3
TheBharatMishra <mishrabm2004@gmail.com>
2024-05-07 14:46:33 +0000

    May 7, 2024, 8:16 PM

---
+++ Algorithms/Recursion/WalkingExample.cpp
@@ -0,0 +1,17 @@
+// Necessary Imports
+#include <iostream>
+
+using namespace std;
+// Define a Class or Method
+void reachHome()
+{
+}
+int main()
+{
+    // Code here.
+    int dist = 10;
+    int src = 1;
+
+    reachHome(src, dist);
+    return 0;
+}
