f797ef67480e8ce91a01dece92eeb8f86af07c69
TheBharatMishra <mishrabm2004@gmail.com>
2024-05-07 16:51:45 +0000

    May 7, 2024, 10:21 PM

--- Algorithms/Recursion/ClimbStaris.cpp
+++ Algorithms/Recursion/ClimbStaris.cpp
@@ -3,7 +3,15 @@
 
 using namespace std;
 // Define a Class or Method
+int countDistinctWaysToClimbStair(long long nStairs)
+{
+    if (nStairs < 0)
+        return 0;
+    if (nStairs == 0)
+        return 1;
 
+    int ans = countDistinctWaysToClimbStair(nStairs - 1) + countDistinctWaysToClimbStair(nStairs - 2);
+}
 int main()
 {
     // Code here.
