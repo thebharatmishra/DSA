c16d147a626856f813de585731e932766577002a
TheBharatMishra <mishrabm2004@gmail.com>
2024-05-09 02:22:35 +0000

    May 9, 2024, 7:52 AM

---
+++ DataStructure/Basic/Basic.cpp
@@ -0,0 +1,14 @@
+// Necessary Imports
+#include <iostream>
+#include <math.h>
+#include <string>
+using namespace std;
+// Define a Class or Method
+
+int main()
+{
+    // Code here.
+    cout << "DioDaint" << endl;
+
+    return 0;
+}
