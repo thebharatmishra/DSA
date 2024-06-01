082f727518a3744feaac3dfbb9f16a64c7b3210e
TheBharatMishra <mishrabm2004@gmail.com>
2024-06-01 15:54:46 +0000

    Jun 1, 2024, 9:24 PM

---
+++ DataStructure/Basic/IfElse.cpp
@@ -0,0 +1,20 @@
+// Necessary Imports
+#include <iostream>
+
+using namespace std;
+// Define a Class or Method
+
+int main()
+{
+    // Code here.
+    int age;
+    if (age < 18)
+    {
+        cout << "Cant vote";
+    }
+    else
+    {
+        cout << "Can Vote";
+    }
+    return 0;
+}
