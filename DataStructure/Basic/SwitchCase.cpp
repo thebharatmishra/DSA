5ead7917e0de881adabfc5dacbe7f21f3fbb503a
TheBharatMishra <mishrabm2004@gmail.com>
2024-06-01 19:19:46 +0000

    Jun 2, 2024, 12:49 AM

---
+++ DataStructure/Basic/SwitchCase.cpp
@@ -0,0 +1,32 @@
+// Necessary Imports
+#include <iostream>
+
+using namespace std;
+// Define a Class or Method
+
+int main()
+{
+    // Code here.
+    int day;
+    cin >> day;
+    switch (day)
+    {
+    case 1:
+        cout << "Monday";
+    case 2:
+        cout << "Tuesday";
+
+    case 3:
+        cout << "Wednesday";
+
+    case 4:
+        cout << "Thursday";
+    case 5:
+        cout << "Friday";
+    case 6:
+        cout << "Saturday";
+    case 7:
+        cout << "Sunday";
+    }
+    return 0;
+}
