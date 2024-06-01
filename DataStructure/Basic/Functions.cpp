ff55b65ea3f25d9b795c4ae746ccf06522862f08
TheBharatMishra <mishrabm2004@gmail.com>
2024-06-01 20:48:09 +0000

    Jun 2, 2024, 2:18 AM

---
+++ DataStructure/Basic/Functions.cpp
@@ -0,0 +1,16 @@
+// Necessary Imports
+#include <iostream>
+
+using namespace std;
+// Define a Class or Method
+void printName(string name)
+{
+
+    cout << name;
+}
+
+int main()
+{
+    // Code here.
+    return 0;
+}
