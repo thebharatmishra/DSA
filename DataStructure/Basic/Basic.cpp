60c074e050752c444844004f515f05ddc4088890
TheBharatMishra <mishrabm2004@gmail.com>
2024-06-01 15:53:35 +0000

    Jun 1, 2024, 9:23 PM

--- DataStructure/Basic/Basic.cpp
+++ DataStructure/Basic/Basic.cpp
@@ -1,6 +1,6 @@
 // Necessary Imports
-#include <iostream>
-// #include <bits/stdc++.h>
+// #include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 // Define a Class or Method
@@ -11,10 +11,14 @@ int main()
     int x, y;
     cout << "DioDaint" << endl;
     cout << "\t DioDaint \n";
-    cin >> x >> y;
+    // cin >> x >> y;
     float bharat69mishra = 69.420;
     long z = 4232424325;
     long long c = 2432222223253233323;
     cout << c;
+    // string and getline
+    string str;
+    getline(cin, str);
+
     return 0;
 }
