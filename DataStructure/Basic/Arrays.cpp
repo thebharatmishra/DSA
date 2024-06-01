4016f8bf483252b5d376d16f9b6d6d907f9682f6
TheBharatMishra <mishrabm2004@gmail.com>
2024-06-01 19:52:48 +0000

    Jun 2, 2024, 1:22 AM

---
+++ DataStructure/Basic/Arrays.cpp
@@ -0,0 +1,13 @@
+// Necessary Imports
+#include <iostream>
+
+using namespace std;
+// Define a Class or Method
+
+int main()
+{
+    // Code here.
+    int arr[4];
+    cin >> arr[0] >> arr[3] >> arr[1] >> arr[2];
+    return 0;
+}
