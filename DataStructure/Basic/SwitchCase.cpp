a171794807d6833ecbad5bd75f3d62f37bf9cbf9
TheBharatMishra <mishrabm2004@gmail.com>
2024-06-01 19:20:43 +0000

    Jun 2, 2024, 12:50 AM

--- DataStructure/Basic/SwitchCase.cpp
+++ DataStructure/Basic/SwitchCase.cpp
@@ -13,18 +13,25 @@ int main()
     {
     case 1:
         cout << "Monday";
+        break;
     case 2:
         cout << "Tuesday";
+        break;
     case 3:
         cout << "Wednesday";
+        break;
     case 4:
         cout << "Thursday";
+        break;
     case 5:
         cout << "Friday";
+        break;
     case 6:
         cout << "Saturday";
+        break;
     case 7:
         cout << "Sunday";
+        break;
     }
     return 0;
 }
