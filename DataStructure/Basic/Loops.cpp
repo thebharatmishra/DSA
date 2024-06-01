5f0029d6fc9fa59e0cf6804fa3e5421dd46dab5f
TheBharatMishra <mishrabm2004@gmail.com>
2024-06-01 19:58:07 +0000

    Jun 2, 2024, 1:28 AM

---
+++ DataStructure/Basic/Loops.cpp
@@ -0,0 +1,16 @@
+// Necessary Imports
+#include <iostream>
+
+using namespace std;
+// Define a Class or Method
+
+int main()
+{
+    // Code here.
+    for (int i = 0; i < 69; i++)
+    {
+        cout << "I love you <3 " << i;
+    }
+
+    return 0;
+}
