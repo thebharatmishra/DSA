c427f882e1517596ba9fe8983465982f935c510b
TheBharatMishra <mishrabm2004@gmail.com>
2024-06-01 19:55:23 +0000

    Jun 2, 2024, 1:25 AM

--- DataStructure/Basic/Arrays.cpp
+++ DataStructure/Basic/Arrays.cpp
@@ -11,7 +11,7 @@ int main()
     cin >> arr[0] >> arr[3] >> arr[1] >> arr[2];
     int a[5][2];
     a[2][2] = 69;
-    cout << a[1][3];
+    cout << a[1][3]; // Garbage value as not defined.
 
     return 0;
 }
