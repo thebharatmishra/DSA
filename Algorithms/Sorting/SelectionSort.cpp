727ce8d67a7eec55b23d658a215ddecab4a1282c
TheBharatMishra <mishrabm2004@gmail.com>
2024-06-03 12:23:51 +0000

    Jun 3, 2024, 5:53 PM

--- Algorithms/Sorting/SelectionSort.cpp
+++ Algorithms/Sorting/SelectionSort.cpp
@@ -2,20 +2,21 @@
 #include <vector>
 using namespace std;
 
-void SelectionSort(vector<int> &arr, int n)
-{
-    for (int i = 0; i < n - 1; i++)
-    {
-        int minIndex = i;
-        for (int j = i + 1; j < n; j++)
-        {
-            if (arr[j] < arr[minIndex])
-                minIndex = j;
-        }
-        swap(arr[minIndex], arr[i]);
-    }
-};
+// void SelectionSort(vector<int> &arr, int n)
+// {
+//     for (int i = 0; i < n - 1; i++)
+//     {
+//         int minIndex = i;
+//         for (int j = i + 1; j < n; j++)
+//         {
+//             if (arr[j] < arr[minIndex])
+//                 minIndex = j;
+//         }
+//         swap(arr[minIndex], arr[i]);
+//     }
+// };
 
+// selecti minimum and swap
 int main()
 {
 
