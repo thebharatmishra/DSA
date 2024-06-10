8376ab64e8bd2c69b8ca054b76b5068b80c26c88
TheBharatMishra <mishrabm2004@gmail.com>
2024-06-10 14:04:52 +0000

    Jun 10, 2024, 7:34 PM

---
+++ DataStructure/Basic/STL.cpp
@@ -0,0 +1,19 @@
+// Necessary Imports
+#include <bits/stdc++.h>
+
+using namespace std;
+// Define a Class or Method
+
+void explainPair()
+{
+    pair<int, int> p = {1, 3};
+    cout << p.first << " " << p.second;
+    pair<int, pair<int, int>> pp = {2, {5, 7}};
+    cout << pp.first << "%" << pp.second.second;
+}
+int main()
+{
+    // Code here.
+
+    return 0;
+}
