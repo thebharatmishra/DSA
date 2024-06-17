7cac19dd54a5e5af88d20ce1825a4c6e7e3a4147
TheBharatMishra <mishrabm2004@gmail.com>
2024-06-17 13:32:41 +0000

    Jun 17, 2024, 7:02 PM

--- Algorithms/DynamicProgramming/FibTUF.cpp
+++ Algorithms/DynamicProgramming/FibTUF.cpp
@@ -11,11 +11,25 @@ int f(int n, vector<int> &dp)
 }
 int main()
 {
+    // Memoization
     int n;
     cin >> n;
     // int dp[n + 1];
     // memset(dp, -1, sizeof dp);
     vector<int> dp(n + 1, -1);
     cout << f(n, dp);
+
+    // Tabulation
+    int m;
+    cin >> m;
+    int prev2 = 0;
+    int prev1 = 1;
+    for (int i = 0; i < m; i++)
+    {
+        int curi = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = curi;
+    }
+
     return 0;
 }
